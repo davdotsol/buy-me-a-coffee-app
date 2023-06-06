d8297429b7e86029a0f95d874fe613628b07de01
davdotsol <davdotsol+github@gmail.com>
2023-06-06 16:16:39 +0200

    Optimized Smart Contract

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -3,26 +3,21 @@ pragma solidity ^0.8.0;
 
 contract BuyMeACoffee {
     address payable public owner;
-    uint public totalDonations;
 
-    event DonationReceived(address indexed donor, uint amount);
+    event DonationReceived(address indexed donor, uint256 donationAmount);
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function donate() public payable {
+    // Donate ETH to the contract owner
+    function donate() external payable {
         require(msg.value > 0, "Donation amount must be greater than zero");
-
-        // Update the total donations
-        totalDonations += msg.value;
 
         // Transfer the donated amount to the contract owner
         owner.transfer(msg.value);
 
-        // Emit the DonationReceived event
+        // Emit the DonationReceived event with the donation details
         emit DonationReceived(msg.sender, msg.value);
-
-        // You can implement additional logic here, such as storing the donor's address and the donation amount
     }
 }
